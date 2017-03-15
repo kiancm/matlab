2fc3ced5896cb0857cd2a052218b7e5d1ea2c024
kianmehrabani <kianonly2@gmail.com>
2017-03-15 10:17:49 -0400

    Added handling for negative numbers and fixed functionality

--- water_tower.m
+++ water_tower.m
@@ -1,10 +1,13 @@
 h = input('h: ');
-rcy = 12.5;
-rco = 23;
-rh = rco + (10.5 / 14) * (h - 19);
-vco = 0;
-if h > 19
-   vco = (1 / 3) * (h - 19) * (rco^2 + rco * rh + rh^2);
+if h >= 0
+    r = 12.5;
+    rh = r + (10.5 / 14) * (h - 19);
+    vco = 0;
+    if h > 19 && h <= 33
+        vco = (1 / 3) * pi * (h - 19) * (r^2 + r * rh + rh^2);
+    end
+    v = pi * r^2 * (h>19) * 19 + (h<=19) * h + vco;
+    fprintf('h: %.3f\nv: %.3f\n',[h,v]);
+else
+    disp('error: input less than 0');
 end
-v = pi * rcy^2 * h + vco;
-fprintf('h: %.3f\nv: %.3f\n',[h,v]);
